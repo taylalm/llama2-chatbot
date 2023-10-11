b71a8ae00904339d9752e327c86314e1bf4b49c6
Thao N. Lam <lamnguyenthanhthao6210@gmail.com>
2023-10-11 16:47:01 +0700

    Update llama2_chatbot.py

--- llama2_chatbot.py
+++ llama2_chatbot.py
@@ -30,7 +30,7 @@ with st.sidebar:
     
     temperature = st.sidebar.slider('temparature', min_value = 0.01, max_value = 5.0, value = 0.1, step = 0.01)
     top_p = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
-    max_length = st.sidebar.slider('max_length', min_value=32, max_value=128, value=120, step=8)
+    max_length = st.sidebar.slider('max_length', min_value=32, max_value=512, value=120, step=8)
 
 #Store LLM generated responses
 if "messages" not in st.session_state.keys():
